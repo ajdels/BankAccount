e2d83331d4aca1518a14675ee2b15bf361c0c6df
anthonydelsignore <adelsignore@student.bridgew.efu>
2021-09-22 15:24:56 -0400

    commit #2 922

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         var myAccount = new BankAccount();
+        var yourAccount = new BankAccount(2000,0.05f);
         myAccount.deposit(1000);
         myAccount.interest();
         var didSuceeed = myAccount.withdraw(2000);
